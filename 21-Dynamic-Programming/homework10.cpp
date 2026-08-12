ecd498eb210635bc5f53abd1b0af43f0f79f3af5
RaghavSemwal <raghav9tha35@gmail.com>
2026-08-12 20:00:51 +0530

    Add homework10.cpp

--- 21-Dynamic-Programming/homework10.cpp
+++ 21-Dynamic-Programming/homework10.cpp
@@ -16,7 +16,7 @@ void printMaxSubarrayIndices(const vector<int>& nums) {
     int maxSum = INT_MIN;
     int start = 0, bestStart = 0, bestEnd = 0;
 
-    for (int i = 0; i < nums.size(); i++) {
+    for (int i = 0; i<nums.size(); i++) {
         if (currentSum == 0) {
             start = i; // Potential new subarray start
         }
